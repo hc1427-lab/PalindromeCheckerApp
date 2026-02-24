fe81e092864719ad2f7aeeeb4e9354c43a3e4729
hc1427-lab <hc1427@srmist.edu.in>
2026-02-24 15:28:36 +0530

    UC6: User input palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -68,5 +68,25 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println(wordUC5 + " is NOT a Palindrome (Stack Method)");
         }
+
+        // UC6 — User Input Palindrome
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String userWord = scanner.nextLine();
+
+        String reversedUC6 = "";
+
+// Reverse the input
+        for (int i = userWord.length() - 1; i >= 0; i--) {
+            reversedUC6 += userWord.charAt(i);
+        }
+
+// Check palindrome
+        if (userWord.equalsIgnoreCase(reversedUC6)) {
+            System.out.println(userWord + " is a Palindrome (User Input)");
+        } else {
+            System.out.println(userWord + " is NOT a Palindrome (User Input)");
+        }
     }
 }
