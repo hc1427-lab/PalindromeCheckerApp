4f3322423c44a1609bd106f9b57b6c7b11c11362
hc1427-lab <hc1427@srmist.edu.in>
2026-02-24 15:27:50 +0530

    UC5: Stack based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -43,5 +43,30 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println(wordUC4 + " is NOT a Palindrome");
         }
+
+
+        // UC5 — Stack Based Palindrome
+        String wordUC5 = "radar";
+
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+// Push characters
+        for (int i = 0; i < wordUC5.length(); i++) {
+            stack.push(wordUC5.charAt(i));
+        }
+
+// Build reversed word using stack
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+// Compare
+        if (wordUC5.equals(reversed)) {
+            System.out.println(wordUC5 + " is a Palindrome (Stack Method)");
+        } else {
+            System.out.println(wordUC5 + " is NOT a Palindrome (Stack Method)");
+        }
     }
 }
