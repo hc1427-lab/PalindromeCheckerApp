ec50bd09928590ab2807bf94c70255a116a02450
hc1427-lab <hc1427@srmist.edu.in>
2026-02-24 15:24:32 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -8,5 +8,21 @@ public class PalindromeCheckerApp {
         System.out.println("Version : 1.0.0");
         System.out.println("Welcome to the Palindrome Checker Application!");
         System.out.println("Program ready...");
+
+        System.out.println("=== PALINDROME CHECKER APP ===");
+
+        // UC2 — Hardcoded Palindrome
+        String word = "madam";
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+
+
+
     }
 }
