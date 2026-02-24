490dd8afef125a0db40159ff743911e63cbfaa49
hc1427-lab <hc1427@srmist.edu.in>
2026-02-24 15:25:20 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,14 +2,13 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        // UC1 — Welcome Message
         System.out.println("=================================");
         System.out.println("     PALINDROME CHECKER APP      ");
         System.out.println("=================================");
         System.out.println("Version : 1.0.0");
         System.out.println("Welcome to the Palindrome Checker Application!");
-        System.out.println("Program ready...");
-
-        System.out.println("=== PALINDROME CHECKER APP ===");
+        System.out.println("Program ready...\n");
 
         // UC2 — Hardcoded Palindrome
         String word = "madam";
@@ -21,8 +20,5 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println(word + " is NOT a Palindrome");
         }
-
-
-
     }
 }
